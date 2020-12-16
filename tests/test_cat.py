3d3c1e1d3a3ba63fafb9e5622683680c61966348
Felipe Rodrigues <felipe@felipevr.com>
2020-12-16 19:40:53 -0300

    [red] Actually, I want Cat to be a strategy-class
    I don't want a simple 'cat' anymore. I want a Cat class that's
    callable and implements different cat-ing strategies!

--- tests/test_cat.py
+++ tests/test_cat.py
@@ -1,7 +1,14 @@
-from pycat import cat
+from pycat import cat, Cat
 
 
 def test_cat_works(capsys):
     cat("tests/hello.txt", "tests/world.txt")
     captured = capsys.readouterr()
     assert captured.out == "hello, world!"
+
+
+def test_cat_strategy(capsys):
+    cat = Cat(strategy="simple")
+    cat("tests/hello.txt")
+    captured = capsys.readouterr()
+    assert captured.out == "hello"
