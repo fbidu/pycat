935b41f5b83b8f8db31fb623ce4a8a87a1f66d1e
Felipe Rodrigues <felipe@felipevr.com>
2020-12-18 09:31:54 -0300

    [green] Implement file adding

--- pycat/recent_used.py
+++ pycat/recent_used.py
@@ -1,3 +1,19 @@
 class RecentUsed:
+    """
+    RecentUsed remembers last recently opened files
+    """
+
+    def __init__(self, data=None):
+        if not data:
+            data = []
+
+        self.data = data
+
     def __len__(self):
         return 0
+
+    def append(self, item):
+        """
+        Adds a new file to the history
+        """
+        self.data.append(item)
