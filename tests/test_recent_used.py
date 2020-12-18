e3ef0ce1c0750f6c1030ac5e47915a4d7393fc24
Felipe Rodrigues <felipe@felipevr.com>
2020-12-18 09:28:38 -0300

    [red] recent used accepts a filename to be remembered

--- tests/test_recent_used.py
+++ tests/test_recent_used.py
@@ -2,10 +2,24 @@ from pycat.recent_used import RecentUsed
 
 
 def test_recent_used_instantiation():
+    """
+    Can we create a new recent used instance?
+    """
     recent_used = RecentUsed()
     assert isinstance(recent_used, RecentUsed)
 
 
 def test_recent_used_initially_empty():
+    """
+    A new RecentUsed instance must be empty
+    """
     recent_used = RecentUsed()
     assert len(recent_used) == 0
+
+
+def test_recent_used_accepts_filename():
+    """
+    Can we register a new 'opened' filename?
+    """
+    recent_used = RecentUsed()
+    recent_used.append("file1")
