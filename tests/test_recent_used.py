22699c729e251ebf1191e63667e4c5dcc2086322
Felipe Rodrigues <felipe@felipevr.com>
2020-12-18 09:36:48 -0300

    Refactor tests
    Now we use Pytest's fixture to provide a RecentUsed instance

--- tests/test_recent_used.py
+++ tests/test_recent_used.py
@@ -1,30 +1,37 @@
 """
 Tests for the RecentUsed class
 """
+import pytest
+
 from pycat.recent_used import RecentUsed
 
+# pylint:disable=redefined-outer-name
+@pytest.fixture
+def recent_used():
+    """
+    Returns a new RecentUsed instance for each test
+    """
+    return RecentUsed()
 
-def test_recent_used_instantiation():
+
+def test_recent_used_instantiation(recent_used):
     """
     Can we create a new recent used instance?
     """
-    recent_used = RecentUsed()
     assert isinstance(recent_used, RecentUsed)
 
 
-def test_recent_used_initially_empty():
+def test_recent_used_initially_empty(recent_used):
     """
     A new RecentUsed instance must be empty
     """
-    recent_used = RecentUsed()
     assert len(recent_used) == 0
 
 
-def test_recent_used_accepts_filename():
+def test_recent_used_accepts_filename(recent_used):
     """
     Can we register a new 'opened' filename?
     """
-    recent_used = RecentUsed()
     recent_used.append("file1")
 
     # after adding, `len` should increase
