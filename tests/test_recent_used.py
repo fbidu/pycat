0bd862b36d5f8156741d4055a396e5d320e6cb33
Felipe Rodrigues <felipe@felipevr.com>
2020-12-18 09:23:47 -0300

    [red] Recent used is initially empty

--- tests/test_recent_used.py
+++ tests/test_recent_used.py
@@ -3,4 +3,9 @@ from pycat.recent_used import RecentUsed
 
 def test_recent_used_instantiation():
     recent_used = RecentUsed()
-    assert recent_used
+    assert isinstance(recent_used, RecentUsed)
+
+
+def test_recent_used_initially_empty():
+    recent_used = RecentUsed()
+    assert len(recent_used) == 0
