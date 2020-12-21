bc3327d12d8c542c27a1a053a3816518fe593ad3
Felipe Rodrigues <felipe@felipevr.com>
2020-12-20 22:07:53 -0300

    [red] test if pycat has history

--- tests/test_cat.py
+++ tests/test_cat.py
@@ -19,3 +19,24 @@ def test_cat_sys_write(capsys):
     cat("tests/hello.txt")
     captured = capsys.readouterr()
     assert captured.out == "hello"
+
+
+def test_cat_keeps_history(capsys):
+    """
+    We want to be able to open a previously opened
+    file by its index:
+
+    >>> cat('file1')
+    <file 1 content>
+    >>> cat('file2')
+    <file 2 content>
+
+    >>> cat('$0')
+    <file 2 content>
+    >> cat('$1')
+    <file 1 content
+    """
+    cat("tests/hello.txt")
+    cat("$0")
+    captured = capsys.readouterr()
+    assert captured.out == "hello, world!"
